1264aad678946fb860144e82dff8e3771f87da86
Furkan Alp <furkanalptokac@gmail.com>
2020-10-29 18:02:42 +0300

    Initial commit

---
+++ functions.js
@@ -0,0 +1,30 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('data.json');
+const db = low(adapter);
+
+const path = './img/elements/';
+const ext = '.gif'
+
+function pressed (btn) {
+    var atomno = document.getElementById('atomno');
+    var adi = document.getElementById('adi');
+    var simge = document.getElementById('simge');
+    var dizilis = document.getElementById('dizilis');
+    var bagtipi = document.getElementById('bagtipi');
+    var sene = document.getElementById('sene');
+    var atomimg = document.getElementById('atomimg');
+    var btnid = parseInt(btn.id);
+    
+    atom = db
+        .find({ atomicNumber: btnid })
+        .value();
+
+    atomno.innerHTML = atom.atomicNumber;
+    adi.innerHTML = atom.name;
+    simge.innerHTML = atom.symbol;
+    dizilis.innerHTML = atom.electronicConfiguration;
+    bagtipi.innerHTML = atom.bondingType;
+    sene.innerHTML = atom.yearDiscovered;
+    atomimg.src = `${path}${btn.id}${ext}`;
+}
---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 850,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    resizable: false
+  })
+
+  win.loadFile('index.html')
+  win.webContents.openDevTools()
+}
+
+try {
+    require('electron-reloader')(module)
+} catch (_) {}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
